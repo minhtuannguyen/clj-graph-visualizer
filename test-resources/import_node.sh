617b6f5bf50866689c50894f16c2bfff89e924bf
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2016-05-17 21:50:56 +0200

    change import script

--- test-resources/import_node.sh
+++ test-resources/import_node.sh
@@ -5,16 +5,23 @@ if [ -z $1 ]; then
   exit 1;
 fi
 
+if [ -z $2 ]; then
+    echo "host is mandatory"	
+    exit 1;
+fi
+
+host=$2
 count=1
 limit=`echo "$1" | bc`
 
-curl -X DELETE http://localhost:8080/node/
+
+curl -X DELETE $host/node/
 
 until [ ! $count -lt  $limit ]
 do
     echo "add node $count"
-	curl -X POST --data "parent=root&id=node$count" http://localhost:8080/node/
-	curl -X POST --data "parent=node$count&id=child$count" http://localhost:8080/node/
+	curl -X POST --data "parent=root&id=node$count" $host/node/
+	curl -X POST --data "parent=node$count&id=child$count" $host/node/
 	count=$((count+1))
 done
 
